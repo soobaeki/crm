b6836ae82c33419d2af0394b890173868e903946
soobaeki <pro.aiden.son@gmail.com>
2026-01-11 12:38:25 +0900

    fixed jwt env error

--- src/lib/jwt/jwt.ts
+++ src/lib/jwt/jwt.ts
@@ -3,12 +3,12 @@ import jwt from "jsonwebtoken";
 // JWT 발급
 const token = jwt.sign(
   { id: 123, role: "superadmin" }, // payload
-  process.env.ENCRYPT_SECRET_KEY, // 비밀 키
+  process.env.ENCRYPT_SECRET_KEY!, // 비밀 키
   { expiresIn: "2h" }, // 만료 시간
 );
 
 console.log(token);
 
 // JWT 검증
-const decoded = jwt.verify(token, process.env.JWT_SECRET);
+const decoded = jwt.verify(token, process.env.JWT_SECRET!);
 console.log(decoded); // { id: 123, role: 'superadmin', iat: ..., exp: ... }
